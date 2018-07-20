d24ad38073272d2c00bbcdf7fbbadc014499104d
Yixuan Sun <yixuan.purdue@gmail.com>
2018-07-20 15:59:37 -0700

    Add files via upload

---
+++ RTS96/extractArea1.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import numpy as np
+import os
+from featureSelection import cleanData
+
+
+path = '/Users/yixuansun/Documents/Research/PNNLrelated/Phase_0_RTS96'
+filenames = os.listdir(path)
+filenames.remove('scorepara.csv')
+filenames.remove('.DS_Store')
+
+
+def convertingToNum(elem):
+	try:
+		elem = float(elem)
+	except ValueError:
+		pass
+	return elem
+
+
+def findlines(scenarioNum):
+	dir_path = path
+	dir_path = os.path.join(dir_path, scenarioNum)
+	txt_file = 'powersystem.raw'
+	headerRow = []
+	startRow = []
+	endRow = []
+	with open(os.path.join(dir_path, txt_file)) as file:
+		for num, line in enumerate(file):
+			line = line.strip()
+			if '/' in line:
+				headerRow.append(num)
+		for start, end in zip(headerRow[:-1], headerRow[1:]):
+			startRow.append(start)
+			endRow.append(end)
+	return	list(np.array(startRow) +1), list(np.array(endRow) - 1)
+
+
+
+def extractCertainLines(startLine, endLine, scenarioNum):
+	dir_path = path
+	dir_path = os.path.join(dir_path, scenarioNum)
+	txt_file = 'powersystem.raw'
+	features = []
+	with open(os.path.join(dir_path, txt_file)) as file:
+		for i, line in enumerate(file):
+			newLine = line.strip().replace(' ','').replace("'","").split(',')
+			newLine = map(convertingToNum, newLine)
+			if i <= endLine - 1 and i >= startLine - 1 and newLine[0] >= 100 and newLine[0] < 200: # area one will start with 1XX
+				features.append(newLine)
+	oneDlist = [item for sublist in features for item in sublist]
+	return oneDlist
+
+
+
+
+
+'''
+-------------
+The following code is to extract contingency info
+-------------
+'''
+def extractContingency(ContNum, scenarioNum):
+	fileDir = os.path.join(path, scenarioNum)
+	file = pd.read_csv(os.path.join(fileDir, 'contingency.csv'))
+	return list(file.iloc[ContNum - 1])
+
+# one contingency for all senarios
+def combineContandFeat(ContNum, scenarioNum):
+	start, end = findlines(scenarioNum)
+	feat = []
+	for i in range(len(start)):
+		feat.append(extractCertainLines(start[i], end[i], scenarioNum))
+	feat.append(extractContingency(ContNum, scenarioNum))
+	feat = [item for i in feat for item in i]
+	return feat
+
+
+
+def ContGenDispatch(ContNum,scenarioNum):
+	_path = os.path.join(path, scenarioNum)
+	file_path = os.path.join(_path, 'solution2.txt')
+	GenDispatch = []
+	with open(file_path, 'r') as f:
+		for i, line in enumerate(f):
+			newLine = line.strip().replace(' ','').replace("'","").split(',')
+			newLine = map(convertingToNum, newLine)
+			if i >=2 and i <= 991 and newLine[0] == ContNum and newLine[2] >= 100 and newLine[2] < 200:
+				GenDispatch.append(newLine[-1])
+	return GenDispatch
+
+
+def feat_target(scenarioNum):
+	sample = []
+	for i in range(10):
+		feat = combineContandFeat(i+1, scenarioNum)
+		target = ContGenDispatch(i+1, scenarioNum)
+		sam = feat + target
+		sample.append(sam)
+	return sample
+
+
+
+# creating files containing 1000 samples
+'''area1Data = []
+for file in filenames:
+		area1Data.append(feat_target(file))
+
+area1FullData = [s for item in area1Data for s in item]
+
+print len(area1FullData), len(area1FullData[0])
+
+
+dataframe = pd.DataFrame(area1FullData)
+
+dataframe.to_csv('area1FullData.csv')'''
+
+data = pd.read_csv('area1FullData.csv', header = 0)
+data = data.iloc[:,1:]
+data = data.dropna(axis = 1)
+cols = data.columns
+num_cols = data._get_numeric_data().columns 
+catData = list(set(cols) - set(num_cols))# detecting categorical features.
+data = data.drop(catData, axis = 1) # dropping all categorical features because they are the same for all samples.
+
+nunique = data.apply(pd.Series.nunique) # find out the repeat data.
+colsToDrop = nunique[nunique == 1].index
+data = data.drop(colsToDrop, axis = 1)# drop out the columns containing the same value.
+data.to_csv('cleanedArea1.csv')
+#df1.to_csv('cleanedArea1.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RTS96/predictiveModel.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+
+filename = 'full_to_area1.csv'
+df = pd.read_csv(filename)
+X = df.iloc[:,:-33]
+print len(X.columns)
+y = df.iloc[:,-33:]
+
+
+
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,random_state = 42)
+scaler = StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+#X_train, explainedVarianceRatio, X_test = principalComponent(X_train, X_test)
+# rf1 = RandomForestRegressor(n_estimators = 500, n_jobs = -1, random_state = 42)
+# rf1.fit(X, y.iloc[:,0])
+
+regressor = MultiOutputRegressor(RandomForestRegressor())
+
+regressor.fit(X_train, y_train)
+
+score = regressor.score(X_test, y_test)
+print score
+
+
+
+# feat_imp = pd.DataFrame({'importance': rf1.feature_importances_})
+# feat_imp['Feature Index'] = X.columns
+# feat_imp.sort_values(by = 'importance', ascending = True, inplace = True)
+
+'''plt.barh(range(len(feat_imp)), feat_imp['importance'], color = 'b', align = 'center')
+plt.yticks(range(len(feat_imp)), feat_imp['Feature Index'], size = 6)
+plt.xlabel('Feature importance')
+plt.ylabel('Index')
+plt.tight_layout()
+
+plt.savefig('feat_imp1.jpg', format = 'jpg', dpi = 500)'''
